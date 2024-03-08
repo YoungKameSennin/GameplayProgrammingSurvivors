a097e8fdb65fd9653131828a99b4f628f561bbbc
ZHZ周浩喆 <zhzzhou@ucdavis.edu>
2024-03-07 18:25:47 -0800

    combat

---
+++ GPSurvivors/Assets/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotateSpeed = 200f;//adjust if needed
+    private Transform target;
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            // let bullet keep track the enemy
+            Vector2 direction = (target.position - transform.position).normalized;
+
+            float rotateAmount = Vector3.Cross(direction, transform.up).z;
+            GetComponent<Rigidbody2D>().angularVelocity = -rotateAmount * rotateSpeed;
+            GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.transform == target)
+        {
+            //这里写扣血逻辑，我不太清楚enemy的构成，不知道该怎么扣血。
+
+            Destroy(gameObject);
+        }
+    }
+}
--- GPSurvivors/Assets/Scripts/PlayerAttackNearestEnemy.cs
+++ GPSurvivors/Assets/Scripts/PlayerAttackNearestEnemy.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAttackNearestEnemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab; 
+    public Transform firePoint;
+    private GameObject currentTarget;
+
     void Update()
     {
+        if (Input.GetButtonDown("Fire1") && GameObject.FindGameObjectWithTag("Enemy") != null)
+        {
+            currentTarget = FindNearestEnemy();
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
         
+        GameObject nearestEnemy = FindNearestEnemy();
+        if (currentTarget != null)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            bullet.GetComponent<Bullet>().SetTarget(currentTarget.transform);
+        }
+    }
+
+    GameObject FindNearestEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject nearest = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < minDistance)
+            {
+                minDistance = distanceToEnemy;
+                nearest = enemy;
+            }
+        }
+        return nearest;
     }
 }
