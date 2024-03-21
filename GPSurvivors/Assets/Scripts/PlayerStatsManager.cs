74d035f7a3ca1966bdbef0d6c024777295bd2e96
YoungKameSennin <shaowei045@gmail.com>
2024-03-21 03:12:18 -0700

    Level up UI

--- GPSurvivors/Assets/Scripts/PlayerStatsManager.cs
+++ GPSurvivors/Assets/Scripts/PlayerStatsManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 public enum UpgradeOption
 {
     IncreaseBullets,
@@ -40,7 +41,9 @@ public class PlayerStatsManager : MonoBehaviour
     public int APbulletCount = 0;
 
 
-
+    public TMP_Text textMesh1;
+    public TMP_Text textMesh2;
+    public TMP_Text textMesh3;
 
 
     void Awake()
@@ -90,7 +93,62 @@ public class PlayerStatsManager : MonoBehaviour
         level++;
 
         List<UpgradeOption> options = GetRandomUpgradeOptions();
-        var retval = GameObject.FindWithTag("LevelUpUI").GetComponent<UIButtonSetter>().setTexts(options);
+        switch (options[0])
+        {
+            case UpgradeOption.IncreaseBullets:
+                textMesh1.text = "Fire Ball + 1";
+                break;
+            case UpgradeOption.IncreaseShieldHealth:
+                textMesh1.text = "Get Shield Enhance";
+                break;
+            case UpgradeOption.IncreaseIceBall:
+                textMesh1.text = "Ice Ball + 1";
+                break;
+            case UpgradeOption.IncreaseMarryGoRound:
+                textMesh1.text = "Marry Go Round + 2";
+                break;
+            case UpgradeOption.IncreaseAPbullet:
+                textMesh1.text = "AP Bullet + 1";
+                break;
+        }
+
+        switch (options[1])
+        {
+            case UpgradeOption.IncreaseBullets:
+                textMesh2.text = "Fire Ball + 1";
+                break;
+            case UpgradeOption.IncreaseShieldHealth:
+                textMesh2.text = "Get Shield Enhance";
+                break;
+            case UpgradeOption.IncreaseIceBall:
+                textMesh2.text = "Ice Ball + 1";
+                break;
+            case UpgradeOption.IncreaseMarryGoRound:
+                textMesh2.text = "Marry Go Round + 2";
+                break;
+            case UpgradeOption.IncreaseAPbullet:
+                textMesh2.text = "AP Bullet + 1";
+                break;
+        }
+
+        switch (options[2])
+        {
+            case UpgradeOption.IncreaseBullets:
+                textMesh3.text = "Fire Ball + 1";
+                break;
+            case UpgradeOption.IncreaseShieldHealth:
+                textMesh3.text = "Get Shield Enhance";
+                break;
+            case UpgradeOption.IncreaseIceBall:
+                textMesh3.text = "Ice Ball + 1";
+                break;
+            case UpgradeOption.IncreaseMarryGoRound:
+                textMesh3.text = "Marry Go Round + 2";
+                break;
+            case UpgradeOption.IncreaseAPbullet:
+                textMesh3.text = "AP Bullet + 1";
+                break;
+        }
         //ApplyUpgrade(1);
     }
 
