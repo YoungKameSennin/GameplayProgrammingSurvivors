9dd379d79377a21d9af9e620188edca985cab680
shijie wei <shijie12655@gmail.com>
2024-03-20 19:21:40 -0700

    jie

--- GPSurvivors/Assets/Scripts/UIExperienceBar.cs
+++ GPSurvivors/Assets/Scripts/UIExperienceBar.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIExperienceBar : MonoBehaviour
 {
     public Slider experienceSlider;
     public PlayerStatsManager statsManager;
+    //public TMP_Text timerText;
+    //private float timer = 0f;
 
     void Start()
     {
         experienceSlider.maxValue = statsManager.maxExperience;
         experienceSlider.value = statsManager.currentExperience;
     }
+    /*
+    void Update()
+    {
+        // Update the timer by adding the time passed since the last frame
+        timer += Time.deltaTime;
+
+        // Format the timer value to display it in minutes and seconds
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = (timer % 60).ToString("00");
+
+        // Update the UI Text component to display the formatted timer value
+        timerText.text = minutes + ":" + seconds;
+    }
+    */
 
     public void SetExperience(float experience)
     {
